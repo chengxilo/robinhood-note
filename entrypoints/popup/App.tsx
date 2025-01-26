e0cfa7dc0962d928041b204c0b2c82d6f029b786
luo-cheng-xi <chengxi.luo2004@gmail.com>
2025-01-26 01:03:35 -0500

    fix: changed the stroage key prefix. feat: add import file refactor: change ui behavior.

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,8 +1,22 @@
 import {
     Button, Container,
-    Stack, Switch, ThemeProvider, Typography, useTheme,
+    Stack, styled, Switch, ThemeProvider, Typography, useTheme,
 } from "@mui/material";
 import genTheme from "@/genTheme.js";
+import {dataStoragePrefix} from "@/storage.ts";
+import {storage, StorageItemKey} from "wxt/storage";
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
 
 function App() {
     const theme = genTheme('dark')
@@ -14,12 +28,14 @@ function App() {
                     direction: 'column',
                     justifyContent: 'center',
                     bgcolor: theme.palette.background.default,
-                    width: '300px',
+                    width: '250px',
                     height: '200px',
                 }}>
-                <Stack sx={{
-                    width: '70%'
-                }}>
+                <Stack
+                    alignItems={'center'}
+                    sx={{
+                        width: '78%'
+                    }}>
                     <Typography
                         color={'secondary'}
                         sx={{
@@ -28,21 +44,74 @@ function App() {
                         Robinhood Note
                     </Typography>
                     <Typography color={'primary'} sx={{
-                        marginBottom: '10px'
+                        marginBottom: '10px',
+                        fontWeight: 'bold'
                     }}>
-                        Download your note with a click!
+                        Manage your note
                     </Typography>
+                    <Button
+                        component={'label'}
+                        variant={'contained'}
+                        sx={{
+                            fontSize: '12px',
+                            marginBottom: '10px',
+                            borderRadius: '44px',
+                            textTransform: 'none',
+                            width: '70%'
+                        }}
+                        color={'secondary'}>
+                        <VisuallyHiddenInput
+                            type="file"
+                            onChange={(event) => {
+                                const fileList = event.target.files
+                                if (fileList === null) {
+                                    console.log('No file selected')
+                                    return
+                                }
+                                const file = fileList[0]
+                                try {
+                                    const reader = new FileReader()
+                                    reader.onload = async function (e) {
+                                        if (e.target === null) {
+                                            console.log('reader.onload, target is null')
+                                            return
+                                        }
+                                        const data = JSON.parse(e.target.result as string)
+                                        console.log(data)
+                                        for (const key1 of Object.keys(data)
+                                            .filter(key => key.startsWith(dataStoragePrefix))) {
+                                            const note = data[key1]
+                                            await storage.setItem(('local:' + key1) as StorageItemKey, note)
+                                        }
+                                        console.log('Imported')
+                                    }
+                                    reader.readAsText(file)
+                                } catch (e) {
+                                    console.error(e)
+                                }
+                            }}
+                        />
+                        Import Note
+                    </Button>
+
                     <Button
                         variant={'contained'}
                         size={'small'}
                         sx={{
                             fontSize: '12px',
+                            borderRadius: '44px',
+                            textTransform: 'none',
+                            width: '70%'
                         }}
                         color={'secondary'}
                         onClick={async () => {
                             const data = await storage.snapshot("local")
 
-                            console.log(data)
+                            // delete all keys that are not note
+                            Object.keys(data)
+                                .filter(key => !key.startsWith(dataStoragePrefix))
+                                .forEach(key => delete data[key])
+
                             // Convert data to JSON string for download
                             const jsonData = JSON.stringify(data);
 
@@ -55,7 +124,7 @@ function App() {
                             link.download = 'data.json'; // Specify the filename
                             link.click(); // Trigger the download
                         }}>
-                        Download Note
+                        Export Note
                     </Button>
                 </Stack>
             </Container>
