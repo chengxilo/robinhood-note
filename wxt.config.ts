33febe06ab6eaa75759543dbec62bdfea2e7f3e6
luo-cheng-xi <chengxi.luo2004@gmail.com>
2025-02-04 00:44:21 -0500

    docs: change name,version,description for publishing

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   extensionApi: 'chrome',
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    permissions: ['storage']
+    name: 'Robinhood Note',
+    description: "A note extension for Robinhood. You can take notes on the Robinhood website with it",
+    permissions: ['storage'],
+    version: '0.0.1'
   }
 });
